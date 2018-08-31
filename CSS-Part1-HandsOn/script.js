7243e99fa953c3ac08aa8b75415dcdc8aaca5e48
Nikos Melikhov <4nikmel@gmail.com>
2018-08-31 16:11:21 -0700

    Added CSS-Part1 HandsOn folder

---
+++ CSS-Part1-HandsOn/script.js
@@ -0,0 +1,8 @@
+var cupsOfFlourCalledFor = 4;
+var currentCups;
+
+for(var i = 0; i < cupsOfFlourCalledFor; i++) {
+  currentCups = 'There are currently ' + i + ' cups of flour in the bowl.';
+}
+
+alert('The bowl has all the flour it needs!');
