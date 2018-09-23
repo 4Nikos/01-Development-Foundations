00e21ef619ab9b6ad2b9c7fa0f5bf48d9be99c82
NikosHP <4nikmel@gmail.com>
2018-09-23 14:56:43 -0700

    Final Project for Flex, Bootstrap, AJAX and JSON is complete

--- L10HandsOn - Flex, Bootstrap, AJAX, JSON/script.js
+++ L10HandsOn - Flex, Bootstrap, AJAX, JSON/script.js
@@ -1,5 +1,5 @@
 let newRequest = new XMLHttpRequest();
-newRequest.onreadystatechange = function() {
+newRequest.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     let myObj = JSON.parse(this.responseText);
     document.getElementById("repo1").innerHTML = myObj[0].html_url;
@@ -9,7 +9,7 @@ newRequest.open("GET", "https://api.github.com/users/4Nikos/repos", true);
 newRequest.send();
 
 let newRequest2 = new XMLHttpRequest();
-newRequest2.onreadystatechange = function() {
+newRequest2.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     let myObj = JSON.parse(this.responseText);
     document.getElementById("repo2").innerHTML = myObj[1].html_url;
